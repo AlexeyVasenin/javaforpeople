731a4a94b091ccb2cc7d8ae9fbb01bea8613728b
Alexey <alexxx1809@gmail.com>
2021-05-20 16:52:03 +0500

    Initial commit

---
+++ src/main/java/ru/vasenin/javaforpeople/JavaforpeopleApplication.java
@@ -0,0 +1,13 @@
+package ru.vasenin.javaforpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaforpeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaforpeopleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vasenin/javaforpeople/JavaforpeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vasenin.javaforpeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaforpeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
